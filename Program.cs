f1dff0f65bbf0b2ccb0773f549fd1fed50dc8034
Vladimir.S <vibiray2@mail.ru>
2023-01-14 01:18:48 +0400

    2 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27. принимаем число и выдаём сумму цифр в числе.
+
+Console.WriteLine("Ведите число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 0) { x = x * -1; } else { x = x * 1; }
+
+int i = 10;
+int sum = 0;
+
+
+int res1 = x % 10;
+//if (res1 < 0) {res1 = res1 * -1;} else {res1 = res1 * 1;}
+Console.Write("последне число = ");
+Console.WriteLine(res1);
+sum = sum + res1;
+
+while (x > i)
+{
+    int res3 = x / i % 10;
+    // if (res3 < 0){res3 = res3 * -1;}else{res3 = res3 * 1;}
+    Console.Write("перед ним = ");
+    Console.WriteLine(res3);
+    sum = sum + res3;
+    i = i * 10;
+}
+
+Console.Write("сумма чисел = ");
+Console.WriteLine(sum);
