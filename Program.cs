073bf7cfd2507f656209bbbf6c28c714bc9ba1eb
Vladimir.S <vibiray2@mail.ru>
2023-01-14 00:50:52 +0400

    1 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25. Напишите цикл: на входе 2 числа (А и В) и возводим число А в натуральную степень В.
+
+Console.WriteLine("Ведите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите число В");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+int i = 0;
+
+if (b<1)
+{
+    Console.WriteLine("значение В должно быть больше или равно 1");
+}
+
+else
+{
+    while (i < b)
+    {
+        res = res * a;
+        //Console.WriteLine(res);    
+        i++;
+    }
+
+    Console.WriteLine(res);
+}
